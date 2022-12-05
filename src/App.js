3a318c29a73cf226f2b39bf0920e8bae9bda8c4d
Kate444ka <dsavchenn@gmail.com>
2022-12-05 12:10:32 +0200

    s

--- src/App.js
+++ src/App.js
@@ -7,6 +7,11 @@ function App() {
     return (
         <div className='App'>
             <div>
+                <img
+                    src='https://testometrika.com/upload/uf/ba1/ba128dca62e8ca4cdaf166addae7afde.svg'
+                    className='image'
+                    alt=''
+                />
                 <h2>Лічильник психологічних захворювань:</h2>
                 <h1>{number}</h1>
                 <button className='minus' onClick={() => setNumber(number - 1)}>
